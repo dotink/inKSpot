3cb71e47cf89ca2b8ab82b9a656ac3ef6a7f4aa5
Matthew J. Sahagian <msahagian@dotink.org>
2011-07-09 14:03:53 -0700

    - Schema changes to support nss

--- support/inkspot.sql
+++ support/inkspot.sql
@@ -1,12 +1,34 @@
 BEGIN;
 
+CREATE SEQUENCE group_id MINVALUE 1000 MAXVALUE 2147483647 NO CYCLE;
+CREATE SEQUENCE user_id MINVALUE 1000 MAXVALUE 2147483647 NO CYCLE;
+
+CREATE TABLE groups (
+	id int4 NOT NULL DEFAULT nextval('group_id'),
+	groupname varchar(64) NOT NULL,
+	description text,
+	login_password varchar(512),
+	PRIMARY KEY id
+);
+
 CREATE TABLE users (
-	id serial PRIMARY KEY,
+	id int4 NOT NULL DEFAULT nextval('user_id'),
 	username varchar(32) NOT NULL UNIQUE,
+	group_id int4 NOT NULL REFERENCES groups(id) ON DELETE RESTRICT ON UPDATE CASCADE,
 	login_password varchar(512) NOT NULL,
 	name varchar(64) NOT NULL,
+	home varchar(512) NOT NULL,
+	shell varchar(512) NOT NULL DEFAULT '/bin/false',
+	last_password_change int4 NOT NULL,
 	request_new_password boolean NOT NULL DEFAULT FALSE,
-	account_expired boolean NOT NULL DEFAULT FALSE
+	account_expired boolean NOT NULL DEFAULT FALSE,
+	PRIMARY KEY id
+);
+
+CREATE TABLE user_groups (
+	group_id int4 NOT NULL REFERENCES groups(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	user_id int4 NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEYT (group_id, user_id)
 );
 
 CREATE TABLE user_settings (
