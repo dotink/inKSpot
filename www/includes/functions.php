ec2abbd8173740d707d08ba8cd5f767a8b304c34
Matthew J. Sahagian <msahagian@dotink.org>
2011-07-19 11:51:15 -0700

    - FIxed function for sexec - Hardcoded appending of inkspot to inkspot groups

--- www/includes/functions.php
+++ www/includes/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-	function sexec($command, &$output, &$return)
+	function sexec($command, &$output = NULL, &$return = NULL)
 	{
 		return exec('/home/inkspot/sbin/' . $command, $output, $return);
 	}
