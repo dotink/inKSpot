14876ae896a165175f272271cb94d9d42f38c2cf
Matthew J. Sahagian <msahagian@dotink.org>
2011-07-19 17:44:37 -0700

    - Fixes to remove

--- remove.sh
+++ remove.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ `ps -A | grep php5-cgi | head 1` ]; then
+if [ `ps -A | grep php5-cgi | head -1` ]; then
 	killall -9 php5-cgi
 fi
 
-if [ `ps -A | grep nginx | head 1` ]; then
+if [ `ps -A | grep nginx | head -1` ]; then
 	killall -9 nginx
 fi
 
@@ -12,7 +12,7 @@ echo "DROP DATABASE inkspot" | sudo -u postgres psql
 echo "DROP USER inkspot" | sudo -u postgres psql
 echo "DROP USER inkspot_ro" | sudo -u postgres psql
 
-rpl " pgsql" "" /etc/nsswitch.conf
+rpl -q " pgsql" "" /etc/nsswitch.conf
 
 rm -rf /home/inkspot
 rm -rf /home/users
