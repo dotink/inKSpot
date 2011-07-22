5412dd7530b20a0657c5887a36c7595054938db7
Matthew J. Sahagian <msahagian@dotink.org>
2011-07-21 19:05:00 -0700

    - Added inKSpot class and configuration

---
+++ www/includes/config/inkspot.php
@@ -0,0 +1,6 @@
+<?php
+
+	return iw::createConfig('global', array(
+		'class'           => 'inKSpot',
+		'external_domain' => 'inkspot'
+	));
---
+++ www/includes/lib/inKSpot.php
@@ -0,0 +1,45 @@
+<?php
+
+	class inKSpot {
+	
+		const EXTERNAL_DOMAIN = 'inkspot';
+		
+		/**
+		 * @static
+		 * @access private
+		 * @var string
+		 * 
+		 * The external domain name for inKSpot
+		 */
+		static private $external_domain = NULL; 
+		
+		/**
+		 * Configures inKSpot
+		 *
+		 * @static
+		 * @access public
+		 * @param array $config The configuration array
+		 * @param string $element The name of the configuration element
+		 * @return boolean TRUE if configuration was successful, FALSE otherwise
+		 */
+		static public function __init($config, $element) {
+			self::$external_domain = isset($config['external_domain'])
+				? $config['external_domain']
+				: self::EXTERNAL_DOMAIN;
+			
+			return TRUE;
+		}
+		
+		/**
+		 * Gets the external domain name for inkSpot
+		 *
+		 * @static
+		 * @access public
+		 * @param void
+		 * @return string The current external domain for inKSpot
+		 */
+		static public function getExternalDomain()
+		{
+			return self::$external_domain;
+		}
+	}
