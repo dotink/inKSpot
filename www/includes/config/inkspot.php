bcd5c63f82b47015dbc25120bd7ffb725ed60d27
Matthew J. Sahagian <msahagian@dotink.org>
2011-07-23 00:32:49 -0700

    - Expanded inKSpot config

--- www/includes/config/inkspot.php
+++ www/includes/config/inkspot.php
@@ -1,6 +1,27 @@
 <?php
 
-	return iw::createConfig('global', array(
-		'class'           => 'inKSpot',
-		'external_domain' => 'inkspot'
+	return iw::createConfig('Custom', array(
+
+		// Don't F'in touch this stuff
+
+		'class'             => 'inKSpot',
+		'preload'           => TRUE,
+		
+		// This should be configured to a value which represents the outside
+		// publically accessible domain name.  If your website is not publically
+		// accessible you can use any valid local domain, so long as your
+		// lookups are right.
+		
+		'external_domain'   => NULL,
+
+		// Users are sometimes prompted with notices and they may be directed
+		// to e-mail you with questions... where should we send it?
+
+		'information_email' => NULL,
+		
+		// Users are sometimes prompted with errors and they may be directed
+		// to e-mail you with support inquiries or bug fixes... where should
+		// we send this one?
+
+		'support_email'     => NULL
 	));
